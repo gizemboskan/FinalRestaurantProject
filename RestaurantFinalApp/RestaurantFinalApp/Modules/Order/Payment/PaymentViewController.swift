7b215e426d6c8af3499bcf4999ea6ca9fe97cf70
gizemboskan <gizem.boskan.dev@gmail.com>
2021-08-16 23:44:38 +0300

    Update PaymentViewController.swift

--- RestaurantFinalApp/RestaurantFinalApp/Modules/Order/Payment/PaymentViewController.swift
+++ RestaurantFinalApp/RestaurantFinalApp/Modules/Order/Payment/PaymentViewController.swift
@@ -14,12 +14,53 @@ class PaymentViewController: UIViewController {
     
     // MARK: - UI Components
     
+    @IBOutlet weak var payButton: UIButton!
+    @IBOutlet weak var kitchenNameLabel: UILabel!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    
+    @IBOutlet weak var creditCardNumberTextField: UITextField!
+    @IBOutlet weak var cashButton: UIButton!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var creditCardCVCTextField: UITextField!
+    @IBOutlet weak var creditCardDateTextField: UITextField!
+    
+    @IBOutlet weak var orderDoneImageView: UIImageView!
+    @IBOutlet weak var orderSucceedView: UIView!
+    @IBOutlet weak var goHomeButton: UIButton!
     // MARK: - UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        orderSucceedView.isHidden = true
+        orderDoneImageView.isHidden = true
+        
+        cashButton.isHighlighted = false
+        creditCardCVCTextField.isEnabled = true
+        creditCardDateTextField.isEnabled = true
+        creditCardNumberTextField.isEnabled = true
     }
     
     // MARK: - Helpers
+    @IBAction func payButtonPressed(_ sender: UIButton) {
+        orderSucceedView.isHidden = false
+        orderDoneImageView.isHidden = true
 
+    }
+    
+    @IBAction func cashButtonPressed(_ sender: UIButton) {
+        cashButton.isHighlighted = true
+        creditCardCVCTextField.isEnabled = false
+        creditCardDateTextField.isEnabled = false
+        creditCardNumberTextField.isEnabled = false
+        
+    }
+    
+    @IBAction func goHomeButtonPressed(_ sender: UIButton) {
+        
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        if let vc = storyboard.instantiateViewController(withIdentifier: "HomePageViewController") as? HomePageViewController {
+            vc.popoverPresentationController?.sourceView = sender as UIView
+            vc.present(vc, animated: true)
+    }
+    }
 }
